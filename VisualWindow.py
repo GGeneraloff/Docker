edcdbb690eab982155ab5edb02010000144efd71
GGeneraloff <generaloffgeorgy@yandex.ru>
2024-05-04 16:40:45 +0300

    Added project

--- VisualWindow.py
+++ VisualWindow.py
@@ -16,10 +16,10 @@ class Visuality:
         self.window.geometry("430x300")
         # check = (self.window.register(self.is_valid), "%P")
         
-        ip_1=tk.StringVar(value='192.168.1.0')
-        ip_2=tk.StringVar(value='192.168.2.0')
-        ip_3=tk.StringVar(value='192.168.3.0')
-        ip_4=tk.StringVar(value='192.168.4.0')
+        self.ip_1=tk.StringVar(value='192.168.1.0')
+        self.ip_2=tk.StringVar(value='192.168.2.0')
+        self.ip_3=tk.StringVar(value='192.168.3.0')
+        self.ip_4=tk.StringVar(value='192.168.4.0')
         
         ip_1_label=ttk.Label(text="IP 1")
         ip_1_label.grid(column=0,row=1,padx=5) 
@@ -30,13 +30,13 @@ class Visuality:
         ip_4_label=ttk.Label(text="IP 4")
         ip_4_label.grid(column=0,row=4,padx=5)
         
-        ip_1_entry=ttk.Entry(textvariable=ip_1, width=10)
+        ip_1_entry=ttk.Entry(textvariable=self.ip_1, width=10)
         ip_1_entry.grid(column=1,row=1,pady=15) 
-        ip_2_entry=ttk.Entry(textvariable=ip_2, width=10)
+        ip_2_entry=ttk.Entry(textvariable=self.ip_2, width=10)
         ip_2_entry.grid(column=1,row=2,pady=15)
-        ip_3_entry=ttk.Entry(textvariable=ip_3, width=10)
+        ip_3_entry=ttk.Entry(textvariable=self.ip_3, width=10)
         ip_3_entry.grid(column=1,row=3,pady=15)
-        ip_4_entry=ttk.Entry(textvariable=ip_4, width=10)
+        ip_4_entry=ttk.Entry(textvariable=self.ip_4, width=10)
         ip_4_entry.grid(column=1,row=4,pady=15)
 
 
@@ -47,8 +47,8 @@ class Visuality:
         btn_test= Button(self.window,width=15, text="Test",command=self.remove)
         btn_test.grid(column=3,row=2)
 
-        btn_satrt= Button(self.window,width=15, text="Запуск", command=self.start)
-        btn_satrt.grid(column=3,row=5)
+        btn_start= Button(self.window,width=15, text="Запуск", command=self.start)
+        btn_start.grid(column=3,row=5)
 
         
  
@@ -64,16 +64,18 @@ class Visuality:
         self.window.mainloop()
 
     def first_start(self):
-        st = os.stat('/home/george/Docker/first_start.sh')
-        os.chmod('/home/george/Docker/first_start.sh', st.st_mode | stat.S_IEXEC)# даем права
-        subprocess.call(['/home/george/Docker/first_start.sh'])
+        first_start = os.path.expanduser('~/Docker/first_start.sh')
+        st = os.stat(first_start)
+        os.chmod(first_start, st.st_mode | stat.S_IEXEC)# даем права
+        subprocess.call([first_start])
         messagebox.showinfo("Первый запуск","Первый запуск завершен")
 
-    def start(self,ip_1,ip_2,ip_3,ip_4):
-        st = os.stat('/home/george/Docker/start.sh')
-        os.chmod('/home/george/Docker/start.sh', st.st_mode | stat.S_IEXEC)# даем права
-        subprocess.call(['/home/george/Docker/start.sh'])
-        messagebox.showinfo("","")
+    def start(self):
+        start = os.path.expanduser( '~/Docker/start.sh' )
+        st = os.stat(start)
+        os.chmod(start, st.st_mode | stat.S_IEXEC)# даем права
+        subprocess.run([start,self.ip_1.get(),self.ip_2.get(),self.ip_3.get(),self.ip_4.get()])
+        # messagebox.showinfo("Настройка окружения","Настройка выполнена")
 
     def remove(self):
         st = os.stat('/home/george/Docker/test.sh')
--- first_start.sh
+++ first_start.sh
@@ -5,6 +5,6 @@ sudo apt-get install -y docker*
 cd ~
 mkdir -p work
 cp -nr ~/Docker/emulator ~/work
-cd work/emulator/container_emul/
+cd ~/work/emulator/container_emul/
 sudo chmod +x restore
 sudo ./restore
