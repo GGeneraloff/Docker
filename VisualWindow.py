43482a5ea25312c3c68c576a7e5bfb7a294de405
GGeneraloff <generaloffgeorgy@yandex.ru>
2024-05-07 12:43:57 +0300

    Added project

--- VisualWindow.py
+++ VisualWindow.py
@@ -13,42 +13,21 @@ class Visuality:
     def workwindow(self):
         self.window = Tk()
         self.window.title("Настройка эмулятора")
-        self.window.geometry("430x300")
+        self.window.geometry("500x300")
         # check = (self.window.register(self.is_valid), "%P")
-        
-        self.ip_1=tk.StringVar(value='192.168.1.0')
-        self.ip_2=tk.StringVar(value='192.168.2.0')
-        self.ip_3=tk.StringVar(value='192.168.3.0')
-        self.ip_4=tk.StringVar(value='192.168.4.0')
-        
-        ip_1_label=ttk.Label(text="IP 1")
-        ip_1_label.grid(column=0,row=1,padx=5) 
-        ip_2_label=ttk.Label(text="IP 2")
-        ip_2_label.grid(column=0,row=2,padx=5)
-        ip_3_label=ttk.Label(text="IP 3")
-        ip_3_label.grid(column=0,row=3,padx=5)
-        ip_4_label=ttk.Label(text="IP 4")
-        ip_4_label.grid(column=0,row=4,padx=5)
-        
-        ip_1_entry=ttk.Entry(textvariable=self.ip_1, width=10)
-        ip_1_entry.grid(column=1,row=1,pady=15) 
-        ip_2_entry=ttk.Entry(textvariable=self.ip_2, width=10)
-        ip_2_entry.grid(column=1,row=2,pady=15)
-        ip_3_entry=ttk.Entry(textvariable=self.ip_3, width=10)
-        ip_3_entry.grid(column=1,row=3,pady=15)
-        ip_4_entry=ttk.Entry(textvariable=self.ip_4, width=10)
-        ip_4_entry.grid(column=1,row=4,pady=15)
+        num=1
+        self.plc_visu(num)
 
 
         btn_first_satrt= Button(self.window,width=15, text="Первый запуск", command=self.first_start)
-        btn_first_satrt.grid(column=3,row=0,sticky=tk.NS)
+        btn_first_satrt.grid(column=1,row=0,sticky=tk.NS)
         # проверить скачан ли докер, создать дериктории(если их нет),загружаем докер контейнера, переход к определению ip
 
         btn_test= Button(self.window,width=15, text="Test",command=self.remove)
-        btn_test.grid(column=3,row=2)
+        btn_test.grid(column=2,row=0)
 
         btn_start= Button(self.window,width=15, text="Запуск", command=self.start)
-        btn_start.grid(column=3,row=5)
+        btn_start.grid(column=3,row=0)
 
         
  
@@ -81,6 +60,36 @@ class Visuality:
         st = os.stat('/home/george/Docker/test.sh')
         os.chmod('/home/george/Docker/test.sh', st.st_mode | stat.S_IEXEC)# даем права
         subprocess.call(['/home/george/Docker/test.sh'])
+
+    def plc_visu(self,num):
+        for i in range(num):
+            n=i
+            self.ip_1=tk.StringVar(value='')
+            self.ip_2=tk.StringVar(value='')
+            self.ip_3=tk.StringVar(value='')
+            self.ip_4=tk.StringVar(value='')
+
+            self.port=tk.StringVar(value='0000')
+        
+            ip_1_label=ttk.Label(text="IP 1")
+            ip_1_label.grid(column=n,row=1,padx=5) 
+            ip_2_label=ttk.Label(text="IP 2")
+            ip_2_label.grid(column=n,row=2,padx=5)
+            ip_3_label=ttk.Label(text="IP 3")
+            ip_3_label.grid(column=n,row=3,padx=5)
+            ip_4_label=ttk.Label(text="IP 4")
+            ip_4_label.grid(column=n,row=4,padx=5)
+            port_label=ttk.Label(text="Port")
+            port_label.grid(column=n+2,row=1)
+            
+            ip_1_entry=ttk.Entry(textvariable=self.ip_1, width=14)
+            ip_1_entry.grid(column=n+1,row=1,pady=15) 
+            ip_2_entry=ttk.Entry(textvariable=self.ip_2, width=14)
+            ip_2_entry.grid(column=n+1,row=2,pady=15)
+            ip_3_entry=ttk.Entry(textvariable=self.ip_3, width=14)
+            ip_3_entry.grid(column=n+1,row=3,pady=15)
+            ip_4_entry=ttk.Entry(textvariable=self.ip_4, width=14)
+            ip_4_entry.grid(column=n+1,row=4,pady=15)
     
     # def is_valid(self,newval):
     #     result=  re.match("^\+{0,1}\d{0,11}$", newval) is not None
--- start.sh
+++ start.sh
@@ -6,8 +6,8 @@ sudo ./del_trei_nets
 
 cd ~/work/emulator/docker_install/
 sudo chmod +x add_trei_nets
-sudo ./add_trei_nets $1 $2 $3 $4
-
+ $1 $2 $3 $4
+sudo ./add_trei_nets
 cp -nr ~/Docker/emulator/example_config_plc/default/run_emul_1 ~/work/emulator
 cp -nr ~/Docker/emulator/example_config_plc/default/run_emul_2 ~/work/emulator
 cp -nr ~/Docker/emulator/example_config_plc/default/run_emul_3 ~/work/emulator
@@ -19,7 +19,9 @@ sudo chmod +x run_emul_3
 sudo chmod +x run_emul_4
 
 sudo ./run_emul_1 $1
-sudo ./run_emul_2
-sudo ./run_emul_3
-sudo ./run_emul_4
+# sudo ./run_emul_2
+# sudo ./run_emul_3
+# sudo ./run_emul_4
+
+# sudo docker stop trei_emul_11
 
